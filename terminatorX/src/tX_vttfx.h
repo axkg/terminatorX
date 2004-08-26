fe219754f85b3629d9b4bd79331c24d08d19c055
Alexander Koenig <alex@lisas.de>
2004-08-26 23:04:42 +0000

    Make it compile with g++ 3.4 - Alex

--- terminatorX/src/tX_vttfx.h
+++ terminatorX/src/tX_vttfx.h
@@ -62,18 +62,18 @@ class vtt_fx
 	void set_vtt(void *v) { vtt=v;}
 	void *get_vtt() { return vtt; }
 	
-	virtual void activate()=NULL;
-	virtual void deactivate()=NULL;
-	virtual void run()=NULL;	
-	virtual int isEnabled()=NULL;
+	virtual void activate()=0;
+	virtual void deactivate()=0;
+	virtual void run()=0;	
+	virtual int isEnabled()=0;
 	virtual void reconnect_buffer();
 	virtual void toggle_drywet();
 	virtual bool is_stereo();
 	virtual tX_drywet_type has_drywet_feature();
 	
-	virtual const char *get_info_string()=NULL;
+	virtual const char *get_info_string()=0;
 	
-	virtual void save(FILE *rc, gzFile rz, char *indent)=NULL;
+	virtual void save(FILE *rc, gzFile rz, char *indent)=0;
 	
 	GtkWidget* get_panel_widget() { return panel_widget; }
 	void set_panel_widget(GtkWidget *widget) { panel_widget=widget; }
