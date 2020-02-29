4b474aa7a98ba464e51077d7250962fa2a986e83
Alexander Koenig <alex@lisas.de>
2020-02-29 12:29:14 +0100

    Merge 28-yelp-tools.patch
    From James Cowgill <jcowgill@debian.org> for the Debian/Ubuntu package.
    https://bugs.debian.org/830028

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 echo Creating files required for building terminatorX...
-gnome-doc-prepare --force --automake
 aclocal -Wno-portability
 autoheader
 automake --add-missing -Wno-portability
